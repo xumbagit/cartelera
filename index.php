376f66a6b1ea9172fc1d2bb0219ef40aa8a243cf
root <root@linux.site>
2014-11-14 11:06:41 -0430

    Cambios Cartelera

--- index.php
+++ index.php
@@ -68,327 +68,50 @@
 		<script src="libs/jquery-2.1.1.js"></script>
 		<script src="libs/jquery.mobile-1.4.4/jquery.mobile-1.4.4.js"></script>
 		<title>Cartelera Virtual</title>
+		<script tyle="text/javascript">
+			$(document).ready(function(){
+				$.ajax({
+					type : "POST",
+					url:'mods/carteleracine.php',
+					dataType : "html",
+					success:function(data){
+						$('#wrapper1').html()=data; // returns cartelera
+					},
+					error:function() {
+						document.write("Sorry, The requested property could not be found.");
+					}
+				});
+				$.ajax({
+					type : "POST",
+					url:'mods/cartelerateatro.php',
+					dataType : "html",
+					success:function(data){
+						$('#wrapper2').html()=data; // returns cartelera
+					},
+					error:function() {
+						document.write("Sorry, The requested property could not be found.");
+					}
+				});
+			});
+		</script>
 	</head>
 	<body>
 		<div data-role="page"> 
 			<div data-role="header" data-position="fixed" data-theme="b">
 				<h1>CARTELERA VIRTUAL</h1>
 			</div> 
-			<div data-role="content" id="contentTotalCartel">
-		<?php
-			if($_GET['slidercine']=="true" || empty($_GET['slidercine']) && empty($_GET['sliderteatro'])){
-		?>
-		<div class="container" id="ContainerTotalCartelera" data-tiempopaglabel="<?php echo($tiempoporpag); ?>" data-tiempocinelabel="<?php echo($tiempo_cine); ?>" data-tiempoteatrolabel="<?php echo($tiempo_teatro); ?>" 
-			data-numpeliculascine="<?php echo($numpeliculas_cine); ?>" data-numpeliculasteatro="<?php echo($numpeliculas_teatro); ?>">
-			<!-- SLIDE PARA CINE !-->
-			<div id="CarteleraSlideCompleta" class="cine slide">
-				<div class="header">
-					<div class="header1">
-						<div class="img_category">
-							<img src="img/logotrasnocho_pq.png" />
-						</div>
-						<div class="title" id="TituloCategoriaCartelera">CARTELERA de CINE</div>
-						<div class="fecha" id="FechaCine"><?php echo(utf8_encode(ucwords(strtolower(strftime($dias[date('w')]." %d "))))."de ".utf8_encode(ucwords(strtolower(strftime($meses[date('n')-1]))))); ?>/</div>
-						<div class="hora" id="HoraCine" style="margin-left:5px;"><?php echo(date("h:i A")); ?></div>
-					</div>
-					<div class="header2">
-						<a href="?slidercine=true">
-							<img src="img/btn_cine.png" />
-						</a><br />
-						<?php
-						
-							if($numpeliculas_teatro>0){
-							?>
-								<a href="?sliderteatro=true">
-									<img src="img/btn_teatro.png" />
-								</a>
-							<?php	
-							}
-						?>
-					</div>
-				</div>
-				<?php
-					//$sqlsel="SELECT COUNT(*) AS total_peliculas, CEIL((COUNT(*)/4)) AS TAMANO_PAGINA, (COUNT(*)/(ABS(CEIL(COUNT(*)/4)))) AS total_paginas FROM peliculas WHERE fecha='".date("Y-m-d")."' AND categoria='CINE' ORDER BY categoria DESC, fecha ASC";
-					//$sqlsel="SELECT * FROM peliculas,horarios WHERE horarios.fecha=CURDATE() AND peliculas.categoria='CINE' GROUP BY peliculas.ID ORDER BY peliculas.categoria DESC, horarios.fecha ASC";
-					$sqlsel="SELECT horarios.fecha, horarios.idpelicula, peliculas.titulo FROM horarios,peliculas WHERE horarios.fecha=CURDATE() AND peliculas.ID=horarios.idpelicula AND peliculas.categoria='CINE' GROUP BY peliculas.ID ORDER BY peliculas.sala ASC";
-					if($dbn->QuerySQL($sqlsel)==0){
-						$filas=$dbn->getFilas();
-						if($filas>0){
-							$datos=$dbn->getData();
-							$total_peliculas=$filas;
-							$TAMANO_PAGINA=6;
-							$total_paginas=(int)abs(ceil($total_peliculas/$TAMANO_PAGINA));
-							$numpeliculas=$total_peliculas;
-						}
-					}
-					//echo("PELICULAS:".$total_peliculas." PAGINAS:".$total_paginas);
-						$i=0;
-						$pagina=1;
-						?>
-						<div class="ventana">
-							<div class="pagina">
-							<?php
-								//Nueva consulta que puede listar las funciones por trasnocho plural y tac
-								//UNICAMENTE PARA TEATRO
-								// SELECT peliculas.ID AS IDpeli, horarios.ID AS IDHora,titulo,descrip,peliculas.sala,censura,precio,(SELECT sala FROM salas WHERE sala = peliculas.sala) AS NombreSala FROM peliculas INNER JOIN horarios ON horarios.fecha = CURDATE() AND peliculas.ID = horarios.idpelicula AND peliculas.categoria = 'TEATRO' INNER JOIN salas sac WHERE sac.tipo = 'TEATRO' GROUP BY peliculas.ID ORDER BY RIGHT(NombreSala,1) DESC LIMIT 0,6
-								// LEGACY
-								// $condic2="SELECT peliculas.ID as IDpeli, horarios.ID as IDHora,titulo,descrip,sala,censura,precio FROM peliculas,horarios WHERE horarios.fecha=CURDATE() AND peliculas.ID=horarios.idpelicula AND peliculas.categoria='CINE' GROUP BY peliculas.ID ORDER BY peliculas.sala ASC LIMIT 0,6";
-								$condic2="SELECT peliculas.ID as IDpeli, horarios.ID as IDHora,titulo,descrip,sala,censura,precio FROM peliculas,horarios WHERE horarios.fecha=CURDATE() AND peliculas.ID=horarios.idpelicula AND peliculas.categoria='CINE' GROUP BY peliculas.ID ORDER BY peliculas.sala ASC"; 
-								if($dbn->QuerySQL($condic2)==0){
-									if($dbn->getFilas()>0){
-										$i=0;
-										while($data=$dbn->getData()){
-											$nomcontenedor="Slidepel".$i;
-											$nomcontpelicula="Slide_ID".$data['IDpeli'];
-											$contcensura="Censura_ID".$data['IDpeli'];
-											$contsala="Sala_ID".$data['IDpeli'];
-											$contprecio="Precio_ID".$data['IDpeli'];
-											$contitulo="Titulo_ID".$data['IDpeli'];
-											$contsubt="Subt_ID".$data['IDpeli'];
-											//$sql="SELECT * FROM horarios WHERE idpelicula='".$data['ID']."' ORDER BY HOUR(horamilitar) ASC";
-											//$sql="SELECT * FROM horarios WHERE idpelicula='".$data['IDpeli']."' AND HOUR(TIME(NOW()))<=HOUR(TIME(horamilitar)) AND fecha='".date("Y-m-d")."' ORDER BY HOUR(horamilitar) ASC";
-											$sql="SELECT * FROM horarios WHERE idpelicula='".$data['IDpeli']."' AND fecha=CURDATE() ORDER BY HOUR(horamilitar) ASC";
-											$dbn2->QuerySQL($sql);
-											$filasu=$dbn2->getFilas();
-											if($filasu>0){
-												?>
-													<div name="<?php echo($nomcontenedor); ?>" id="<?php echo($nomcontenedor); ?>">
-														<div class="slideshow" id="<?php echo($nomcontpelicula); ?>">
-															<div class="nombre">
-																<div class="principal" id="<?php echo(contitulo); ?>"><?php echo(utf8_encode($data['titulo'])); ?></div>
-																<div class="sub" id="<?php echo(contsubt); ?>"><?php echo(strip_tags(utf8_encode($data['descrip']))); ?></div>
-															</div>
-															<div id="<?php echo($nombreelim1); ?>" class="sep"></div>
-															<div id="<?php echo($nombreelim2); ?>" class="horario">
-															<?php
-															if($filasu>0){
-																$horariostodos=$dbn2->getAllData();
-																for($u=0;$u<=($filasu-1);$u++){
-																	$shrink=explode(":",$horariostodos[$u]['horacompleta']);
-																	$nhora=$shrink[0].":".$shrink[1];
-																	$nombreblink="blink_".$horariostodos[$u]['ID'];
-																	$nombreelim1="elim1_".$horariostodos[$u]['ID'];
-																	$nombreelim2="elim2_".$horariostodos[$u]['ID'];
-																	$nombreelim3="elim3_".$horariostodos[$u]['ID'];
-																	$nombreelim4="elim4_".$horariostodos[$u]['ID'];
-																	$nombreelim5="elim5_".$horariostodos[$u]['ID'];
-																	$nombreelim6="elim6_".$horariostodos[$u]['ID'];
-																	?>
-													                	<div id="<?php echo($nombreelim3); ?>" class="hora"><?php echo($nhora); ?></div>
-													                	<div id="<?php echo($nombreblink); ?>" class="blink" style="display:none;"></div>
-												                		<div id="<?php echo($nombreelim5); ?>" class="agotado" style="display:none;"><?php echo("AGOTADO"); ?></div>
-																	<?php
-																}
-															}
-															?>
-															</div>
-															<div class="sep"></div>
-															<div class="sala" id="<?php echo($contsala); ?>">
-																<div id="<?php echo($nombreelim6); ?>" class="text">censura <span style="font-weight: bold;"><?php echo($data['censura']); ?></span></div>
-																SALA<br/><?php echo($data['sala']); ?>
-															</div>
-															<!-- <div class="sep"></div> -->
-														</div>
-													</div>
-												<?php
-												$i++;
-											}
-										}
-									}
-								}
-							?>
-							</div>
-						</div>
-				</div>
-				<?php
-				}
-				if($_GET['sliderteatro']=="true"){
-					?>
-						<!-- SLIDE PARA TEATRO !-->
-						<div id="CarteleraSlideCompletaTeatro" class="teatro slide">
-							<?php
-								// $sqlsel="SELECT COUNT(*) AS total_peliculas, 4 AS TAMANO_PAGINA, ABS(CEIL(COUNT(*)/4)) AS total_paginas FROM peliculas WHERE fecha='".date("Y-m-d")."' AND categoria='TEATRO' ORDER BY categoria DESC, fecha ASC";
-								// if($dbn->QuerySQL($sqlsel)==0){
-									// if($dbn->getFilas()>0){
-										// $datos=$dbn->getData();
-										// $total_peliculas1=$datos['total_peliculas'];
-										// $TAMANO_PAGINA1=$datos['TAMANO_PAGINA'];
-										// $total_paginas1=(int)$datos['total_paginas'];
-										// $numpeliculas1=$total_peliculas;
-									// }
-								// }
-								$sqlsel="SELECT * FROM peliculas,horarios,salas WHERE horarios.fecha=CURDATE() AND peliculas.categoria='TEATRO' GROUP BY peliculas.ID ORDER BY peliculas.categoria DESC, horarios.fecha ASC,salas.ID ASC";
-								if($dbn->QuerySQL($sqlsel)==0){
-									$filas=$dbn->getFilas();
-									if($filas>0){
-										$datos=$dbn->getData();
-										$total_peliculas1=$filas;
-										$TAMANO_PAGINA1=6;
-										$total_paginas1=(int)abs(ceil($total_peliculas1/$TAMANO_PAGINA1));
-										$numpeliculas1=$total_peliculas1;
-									}
-								}
-							?>
-							<div class="header">
-								<div class="header1">
-									<div class="img_category">
-										<img src="img/logotrasnocho_pq.png" />
-									</div>
-									<div class="title" id="TituloCategoriaCartelera">CARTELERA de TEATRO</div>
-									<div class="fecha" id="FechaCine"><?php echo(utf8_encode(ucwords(strtolower(strftime($dias[date('w')]." %d "))))."de ".utf8_encode(ucwords(strtolower(strftime($meses[date('n')-1]))))); ?> / </div>
-									<div class="hora" id="HoraCine"><?php echo(date("h:i A")); ?></div>
-								</div>
-								<div class="header2">
-									<a href="?slidercine=true">
-										<img src="img/btn_cine.png" />
-									</a><br />
-									<?php
-										if($numpeliculas_teatro>0){
-										?>
-											<a href="?sliderteatro=true">
-												<img src="img/btn_teatro.png" />
-											</a>
-										<?php	
-										}
-									?>
-								</div>
-							</div>
-							<?php
-								$nomcontenedor="SlidepelT";
-								$i=0;
-								$pagina=1;
-							?>
-								<div class="ventana">
-									<div class="pagina">
-									<?php
-									//SELECT peliculas.ID AS IDpeli, horarios.ID AS IDHora,titulo,descrip,peliculas.sala,censura,precio,(SELECT sala FROM salas WHERE sala = peliculas.sala) AS NombreSala FROM peliculas INNER JOIN horarios ON horarios.fecha = CURDATE() AND peliculas.ID = horarios.idpelicula AND peliculas.categoria = 'TEATRO' INNER JOIN salas sac WHERE sac.tipo = 'TEATRO' GROUP BY peliculas.ID ORDER BY RIGHT(NombreSala,1) DESC LIMIT 0,6
-									//LEGACY
-									//SELECT peliculas.ID as IDpeli, horarios.ID as IDHora,titulo,descrip,peliculas.sala,censura,precio, salas.ID FROM peliculas,horarios,salas WHERE horarios.fecha=CURDATE() AND peliculas.ID=horarios.idpelicula AND peliculas.categoria='TEATRO' AND salas.tipo='TEATRO' GROUP BY peliculas.ID ORDER BY salas.ID ASC LIMIT 0,6
-									$condic2="SELECT peliculas.ID AS IDpeli, horarios.ID AS IDHora,titulo,descrip,peliculas.sala,censura,precio,(SELECT sala FROM salas WHERE sala = peliculas.sala) AS NombreSala FROM peliculas INNER JOIN horarios ON horarios.fecha = CURDATE() AND peliculas.ID = horarios.idpelicula AND peliculas.categoria = 'TEATRO' INNER JOIN salas sac WHERE sac.tipo = 'TEATRO' GROUP BY peliculas.ID ORDER BY RIGHT(NombreSala,1) DESC";
-									if($dbn->QuerySQL($condic2)==0){
-										if($dbn->getFilas()>0){
-											$i=0;
-											while($data=$dbn->getData()){
-												$nomcontenedor="Slidepel".$i;
-												$nomcontpelicula="Slide_ID".$data['IDpeli'];
-												$contcensura="Censura_ID".$data['IDpeli'];
-												$contsala="Sala_ID".$data['IDpeli'];
-												$contprecio="Precio_ID".$data['IDpeli'];
-												$contitulo="Titulo_ID".$data['IDpeli'];
-												$contsubt="Subt_ID".$data['IDpeli'];
-												//$sql="SELECT * FROM horarios WHERE idpelicula='".$data['ID']."' AND HOUR(TIME(NOW()))<=HOUR(TIME(horamilitar)) ORDER BY HOUR(horamilitar) ASC";
-												//$sql="SELECT * FROM horarios WHERE idpelicula='".$data['ID']."' ORDER BY HOUR(horamilitar) ASC";
-												$sql="SELECT * FROM horarios WHERE idpelicula='".$data['IDpeli']."' AND fecha=CURDATE() ORDER BY HOUR(horamilitar) ASC";
-												$dbn2->QuerySQL($sql);
-												$filasu=$dbn2->getFilas();
-												if($filasu>0){
-													?>
-														<div name="<?php echo($nomcontenedor); ?>" id="<?php echo($nomcontenedor); ?>">
-															<div class="slideshow" id="<?php echo($nomcontpelicula); ?>">
-																<div class="nombre">
-																	<div class="principal" id="<?php echo(contitulo); ?>"><?php echo(utf8_encode($data['titulo'])); ?></div>
-																	<div class="sub" id="<?php echo(contsubt); ?>"><?php echo(strip_tags(utf8_encode($data['descrip']))); ?></div>
-																</div>
-																<div id="<?php echo($nombreelim1); ?>" class="sep"></div>
-																<div id="<?php echo($nombreelim2); ?>" class="horario">
-																<?php
-																if($filasu>0){
-																	$horariostodos=$dbn2->getAllData();
-																	for($u=0;$u<=($filasu-1);$u++){
-																		$shrink=explode(":",$horariostodos[$u]['horacompleta']);
-																		$nhora=$shrink[0].":".$shrink[1];
-																		$nombreblink="blink_".$horariostodos[$u]['ID'];
-																		$nombreelim1="elim1_".$horariostodos[$u]['ID'];
-																		$nombreelim2="elim2_".$horariostodos[$u]['ID'];
-																		$nombreelim3="elim3_".$horariostodos[$u]['ID'];
-																		$nombreelim4="elim4_".$horariostodos[$u]['ID'];
-																		$nombreelim5="elim5_".$horariostodos[$u]['ID'];
-																		$nombreelim6="elim6_".$horariostodos[$u]['ID'];
-																		?>
-														                	<div id="<?php echo($nombreelim3); ?>" class="hora"><?php echo($nhora); ?></div>
-														                	<div id="<?php echo($nombreblink); ?>" class="blink" style="display:none;"></div>
-													                		<div id="<?php echo($nombreelim5); ?>" class="agotado" style="display:none;"><?php echo("AGOTADO"); ?></div>
-																		<?php
-																	}
-																}
-																?>
-																</div>
-																<div class="sep"></div>
-																<div class="sala" id="<?php echo($contsala); ?>" style="margin-top:15px;margin-left:5px;">
-																	<div id="<?php echo($nombreelim6); ?>" class="text">censura <span style="font-weight: bold;"><?php echo($data['censura']); ?></span></div>
-																	<span style="font-size: 30px;line-height: 32px;">SALA <?php echo($data['sala']); ?></span>
-																	<div class="text"><span id="<?php echo($contprecio); ?>" class="letras" style="font-size:35px;line-height: 20px;"><?php echo(number_format($data['precio'], 2, ',', '.')); ?></span></div>
-																</div>
-																<!-- <div class="sep"></div> -->
-															</div>
-														</div>
-													<?php
-													$i++;
-												}
-											}
-										}
-									}
-								?>
-							</div>
-						</div>
-					</div>
-				<?php
-				}
-
-				if($_GET['idpelicula']!=''){
-					?>
-						<div class="content_detalles_peli">
-							<div class="header">
-								<div class="header1">
-									<div class="tit">
-										<span><!-- TITULO !--></span>
-									</div>
-									<div class="sub">
-										<span><!-- SUBTITULO !--></span>
-									</div>
-								</div>
-								<div class="header2">
-									<div class="censura">
-										<span><!-- CENSURA !--></span>
-									</div>
-									<div class="sala">
-										SALA<br><span><!-- SALA !--></span>
-									</div>
-								</div>
-								<div class="content_in">
-									<div class="horarios_div">
-										
-									</div>
-									<div class="director">
-										<span><!-- DIRECTOR !--></span>
-									</div>
-									<div class="director">
-										<span><!-- DIRECTOR !--></span>
-									</div>
-									<div class="protagonistas">
-										<span><!-- PROTAGONISTAS !--></span>
-									</div>
-									<div class="protagonistas">
-										<span><!-- PROTAGONISTAS !--></span>
-									</div>
-									<div class="sinopsis">
-										<span><!-- SINOPSIS !--></span>
-									</div>
-									<div class="sinopsis">
-										<span><!-- SINOPSIS !--></span>
-									</div>
-									<div class="trailer_btn">
-										<span>VER TRAILER<!-- VER TRAILER !--></span>
-									</div>
-								</div>
-							</div>
-						</div>
-					
-					<?php
-				}
-			?>
-		</div>
+			<div data-role="content" id="wrapper1"></div> 
+			<div data-role="footer" data-theme="b" data-position="fixed">
+				<h1>
+					Dise&ntilde;o y programaci&oacute;n Xumba de Venezuela 2014
+				</h1>
 			</div> 
+		</div>
+		<div data-role="page"> 
+			<div data-role="header" data-position="fixed" data-theme="b">
+				<h1>CARTELERA VIRTUAL</h1>
+			</div> 
+			<div data-role="content" id="wrapper2"></div> 
 			<div data-role="footer" data-theme="b" data-position="fixed">
 				<h1>
 					Dise&ntilde;o y programaci&oacute;n Xumba de Venezuela 2014
